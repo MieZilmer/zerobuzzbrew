2d68b661b77547918a2fd6b7fbbe22a248327ff9
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2024-05-20 16:21:56 +0200

    Navbar og favicon ish

---
+++ app.js
@@ -0,0 +1,14 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        // Toggle nav
+        nav.classList.toggle('nav-active');
+
+        // Burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
