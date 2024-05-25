e188421a38367549916533aa1601f1501face7ef
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2024-05-25 15:17:01 +0200

    Burgerbaren virker igen

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const burgerDivs = document.querySelectorAll('.burger div');
 
     burger.addEventListener('click', () => {
         // Toggle nav
@@ -8,20 +9,12 @@ const navSlide = () => {
 
         // Burger animation
         burger.classList.toggle('toggle');
+
+        // Apply transition to each div inside the burger
+        burgerDivs.forEach((div) => {
+            div.style.transition = 'all 0.6s ease';
+        });
     });
 }
 
-navSlide();
-
-var burger = document.querySelector('.burger');
-
-burger.addEventListener('click', function() {
-    var burgerDivs = document.querySelectorAll('.burger div');
-
-    burgerDivs.forEach(function(div) {
-        div.style.transition = 'all 0.6s ease';
-    });
-
-
-
-
+navSlide(); // Don't forget to call the function
