fd0d7e1da04d1134081fffd0d5b824a528544856
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2024-06-01 05:12:36 +0200

    Mobile querie done

--- app.js
+++ app.js
@@ -24,5 +24,30 @@ window.addEventListener('load', function() {
     document.querySelector('.forside-billede').classList.add('loaded');
 });
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  if (window.innerWidth <= 768) { // Check if the window's width is less than or equal to 768px
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector("nav").style.top = "0";
+    } else {
+      document.querySelector("nav").style.top = "-8vh";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+}
 
 
+// Function to show the nav links and disable body and html scroll
+function showNavLinks() {
+  navLinks.style.transform = 'translateX(0)';
+  document.body.style.overflow = 'hidden';
+  document.documentElement.style.overflow = 'hidden'; // Add this line
+}
+
+// Function to hide the nav links and enable body and html scroll
+function hideNavLinks() {
+  navLinks.style.transform = 'translateX(100%)';
+  document.body.style.overflow = '';
+  document.documentElement.style.overflow = ''; // Add this line
+}
